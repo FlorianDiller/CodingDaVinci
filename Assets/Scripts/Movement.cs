47cd52d557bc5a417c9fb9fcdb03b5be16344941
Florian Diller <diller@hs-worms.de>
2022-05-10 11:34:51 +0200

    Working movement physics Added Terrain Plane modelled

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour
     [SerializeField]
     public float speed;
     public float rotSpeed;
+    public float tiltAngle = 0.3f;
     //public Vector2 direction;
     // Start is called before the first frame update
     void Start()
@@ -20,8 +21,25 @@ public class Movement : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
         gameObject.transform.Rotate(0.0f,rotSpeed*h,0.0f);
-        speed = speed + v;
+        speed = speed + v * 0.01f;
         gameObject.transform.position = gameObject.transform.position + speed * gameObject.transform.forward;
-        gameObject.transform.GetChild(0).Rotate(0.0f,0.0f,rotSpeed * h);
+        
+        if (Input.GetAxisRaw("Horizontal") != 0 && Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.z) < tiltAngle)
+        {
+            gameObject.transform.GetChild(0).transform.Rotate(0, 0, -0.2f* Input.GetAxisRaw("Horizontal"));
+            Debug.Log(gameObject.transform.GetChild(0).transform.localRotation.z);
+        }
+        else
+        {
+            if (gameObject.transform.GetChild(0).transform.localRotation.z>0)
+            {
+                gameObject.transform.GetChild(0).transform.Rotate(0, 0, -0.2f);
+            }
+            else
+            {
+                gameObject.transform.GetChild(0).transform.Rotate(0, 0, 0.2f);
+            }
+        }
+        
     }
 }

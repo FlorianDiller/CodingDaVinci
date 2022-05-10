750cc59390667ec5b834e6c9b09aa3d3bba69da6
Florian Diller <diller@hs-worms.de>
2022-05-10 14:45:06 +0200

    Added Coloured Terrain Ascent/Descent possible now New plane

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,9 @@ public class Movement : MonoBehaviour
     [SerializeField]
     public float speed;
     public float rotSpeed;
+    public float liftSpeed;
     public float tiltAngle = 0.3f;
+    public float minHeight = 85f;
     //public Vector2 direction;
     // Start is called before the first frame update
     void Start()
@@ -20,16 +22,16 @@ public class Movement : MonoBehaviour
     {
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
-        gameObject.transform.Rotate(0.0f,rotSpeed*h,0.0f);
         speed = speed + v * 0.01f;
         gameObject.transform.position = gameObject.transform.position + speed * gameObject.transform.forward;
-        
+
+        //Rotation and tilting while doing so
+        gameObject.transform.Rotate(0.0f, rotSpeed * h, 0.0f);
         if (Input.GetAxisRaw("Horizontal") != 0 && Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.z) < tiltAngle)
         {
             gameObject.transform.GetChild(0).transform.Rotate(0, 0, -0.2f* Input.GetAxisRaw("Horizontal"));
-            Debug.Log(gameObject.transform.GetChild(0).transform.localRotation.z);
         }
-        else
+        else //Tilt back to normal
         {
             if (gameObject.transform.GetChild(0).transform.localRotation.z>0)
             {
@@ -40,6 +42,32 @@ public class Movement : MonoBehaviour
                 gameObject.transform.GetChild(0).transform.Rotate(0, 0, 0.2f);
             }
         }
-        
+
+        //Ascent and Descent
+        if (Input.GetKey("space"))
+        {
+            gameObject.transform.position = gameObject.transform.position + liftSpeed * gameObject.transform.up;
+            if (Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.x) < tiltAngle)
+            {
+                gameObject.transform.GetChild(0).transform.Rotate(-0.2f, 0, 0);
+            }
+        }
+        if (Input.GetKey("c") && gameObject.transform.position.y > minHeight)
+        {
+            if (Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.x) < tiltAngle)
+            {
+                gameObject.transform.GetChild(0).transform.Rotate(0.2f, 0, 0);
+            }
+            gameObject.transform.position = gameObject.transform.position + liftSpeed * -gameObject.transform.up;
+        }
+        //Tilt back to normal
+        if (gameObject.transform.GetChild(0).transform.localRotation.x > 0 && !Input.GetKey("c"))
+        {
+            gameObject.transform.GetChild(0).transform.Rotate(-0.2f,0, 0);
+        }
+        if (gameObject.transform.GetChild(0).transform.localRotation.x < 0 && !Input.GetKey("space"))
+        {
+                gameObject.transform.GetChild(0).transform.Rotate(0.2f,0, 0);
+        }
     }
 }

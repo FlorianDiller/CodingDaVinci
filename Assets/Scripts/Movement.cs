ad1aa972706a26ad8b074ca41ef1f05f1b3ab55a
DESKTOP-QLMFQES\fmdil <fmdiller@gmail.com>
2022-05-16 19:30:14 +0200

    fitted the speeds to Time.delayTime

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -25,21 +25,22 @@ public class Movement : MonoBehaviour
         speed = speed + v * 0.01f;
         gameObject.transform.position = gameObject.transform.position + 0.1f * speed * gameObject.transform.forward;
 
-        //Rotation and tilting while doing so
-        gameObject.transform.Rotate(0.0f, rotSpeed * h, 0.0f);
+        //Turning and tilting while doing so
+        gameObject.transform.Rotate(0.0f, rotSpeed * h, 0.0f * Time.deltaTime);
         if (Input.GetAxisRaw("Horizontal") != 0 && Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.z) < tiltAngle)
         {
-            gameObject.transform.GetChild(0).transform.Rotate(0, 0, -0.2f* Input.GetAxisRaw("Horizontal"));
+            gameObject.transform.GetChild(0).transform.Rotate(0, 0, -75 * Input.GetAxisRaw("Horizontal") * Time.deltaTime);
         }
+
         else //Tilt back to normal
         {
             if (gameObject.transform.GetChild(0).transform.localRotation.z>0)
             {
-                gameObject.transform.GetChild(0).transform.Rotate(0, 0, -0.2f);
+                gameObject.transform.GetChild(0).transform.Rotate(0, 0, -75 * Time.deltaTime);
             }
             else
             {
-                gameObject.transform.GetChild(0).transform.Rotate(0, 0, 0.2f);
+                gameObject.transform.GetChild(0).transform.Rotate(0, 0, 75 * Time.deltaTime);
             }
         }
 
@@ -49,25 +50,25 @@ public class Movement : MonoBehaviour
             gameObject.transform.position = gameObject.transform.position + liftSpeed * gameObject.transform.up;
             if (Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.x) < tiltAngle)
             {
-                gameObject.transform.GetChild(0).transform.Rotate(-0.2f, 0, 0);
+                gameObject.transform.GetChild(0).transform.Rotate(-75 * Time.deltaTime, 0, 0);
             }
         }
         if (Input.GetKey("c") && gameObject.transform.position.y > minHeight)
         {
             if (Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.x) < tiltAngle)
             {
-                gameObject.transform.GetChild(0).transform.Rotate(0.2f, 0, 0);
+                gameObject.transform.GetChild(0).transform.Rotate(75 * Time.deltaTime, 0, 0);
             }
-            gameObject.transform.position = gameObject.transform.position + liftSpeed * -gameObject.transform.up;
+            gameObject.transform.position = gameObject.transform.position + liftSpeed * 75 * -gameObject.transform.up * Time.deltaTime;
         }
         //Tilt back to normal
         if (gameObject.transform.GetChild(0).transform.localRotation.x > 0 && !Input.GetKey("c"))
         {
-            gameObject.transform.GetChild(0).transform.Rotate(-0.2f,0, 0);
+            gameObject.transform.GetChild(0).transform.Rotate(-75 * Time.deltaTime,0, 0);
         }
         if (gameObject.transform.GetChild(0).transform.localRotation.x < 0 && !Input.GetKey("space"))
         {
-                gameObject.transform.GetChild(0).transform.Rotate(0.2f,0, 0);
+                gameObject.transform.GetChild(0).transform.Rotate(75 * Time.deltaTime,0, 0);
         }
     }
 }

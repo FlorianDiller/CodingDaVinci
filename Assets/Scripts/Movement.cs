cb110accfaed85c1fd88afa206052fd2ed776cf1
Florian Diller <diller@hs-worms.de>
2022-05-19 15:52:36 +0200

    Added boundary not to fly through created more clouds, so no edge is showing

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-    public float speed;
+    public float speed = 0.1f;
+    public float minSpeed = 0.05f;
+    public float maxSpeed = 5f;
     public float rotSpeed;
     public float liftSpeed;
     public float tiltAngle = 0.3f;
-    public float minHeight = 85f;
+    public float minHeight = 50f;
+    public float maxHeight = 400f;
+    public float boundary = 500;
     //public Vector2 direction;
     // Start is called before the first frame update
     void Start()
@@ -22,16 +26,28 @@ public class Movement : MonoBehaviour
     {
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
-        speed = speed + v * 0.01f;
+        speed = Mathf.Clamp(speed + v * 0.01f,minSpeed,maxSpeed);
         gameObject.transform.position = gameObject.transform.position + 0.1f * speed * gameObject.transform.forward;
 
-        //Turning and tilting while doing so
-        gameObject.transform.Rotate(0.0f, rotSpeed * h, 0.0f * Time.deltaTime);
+        //Turning //Boundary check, turns if not viable
+        if (Mathf.Abs(gameObject.transform.position.x - 500) < boundary && Mathf.Abs(gameObject.transform.position.z - 500) < boundary)
+        {
+            gameObject.transform.Rotate(0.0f, rotSpeed * h, 0.0f * Time.deltaTime);
+        }
+        else
+        {
+            gameObject.transform.Rotate(0.0f, rotSpeed, 0.0f * Time.deltaTime);
+            if (Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.z) < tiltAngle)
+            {
+                gameObject.transform.GetChild(0).transform.Rotate(0, 0, -150 * Time.deltaTime);
+            }
+        }
+
+        //Tilting while turning
         if (Input.GetAxisRaw("Horizontal") != 0 && Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.z) < tiltAngle)
         {
             gameObject.transform.GetChild(0).transform.Rotate(0, 0, -75 * Input.GetAxisRaw("Horizontal") * Time.deltaTime);
         }
-
         else //Tilt back to normal
         {
             if (gameObject.transform.GetChild(0).transform.localRotation.z>0)
@@ -45,7 +61,7 @@ public class Movement : MonoBehaviour
         }
 
         //Ascent and Descent
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && gameObject.transform.position.y < maxHeight)
         {
             gameObject.transform.position = gameObject.transform.position + liftSpeed * gameObject.transform.up;
             if (Mathf.Abs(gameObject.transform.GetChild(0).transform.localRotation.x) < tiltAngle)
